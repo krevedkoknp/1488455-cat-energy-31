4e045fa23ade51eec7373d7a70b7f874ba894e17
krevedkoknp <krevedko.knp@gmail.com>
2024-05-17 22:49:22 +0300

    добавляет стили в хедер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navigation = document.querySelector('.navigation');
+let navigationToggle = document.querySelector('.navigation__toggle');
+
+navigation.classList.remove('navigation--nojs');
+
+navigationToggle.addEventListener('click', function () {
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--opened');
+  } else {
+    navigation.classList.add('navigation--closed');
+    navigation.classList.remove('navigation--opened');
+  }
+});
